a93f1bc9358827e7c0b2207f803bba7cfc819cf7
Freedy Carvalho <freedy.carvalho@mk2.com.br>
2021-07-13 17:46:53 -0300

    Publicando

---
+++ Sorteio/Entities/Participante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorteio.Entities
+{
+    class Participante
+    {
+        public int ParticipanteID { get; set; }
+        public string Nome { get; set; }
+
+        public Participante(int participanteID, string nome)
+        {
+            ParticipanteID = participanteID;
+            Nome = nome;
+        }
+
+        public override string ToString()
+        {
+            return ParticipanteID + " - " + Nome;
+        }
+    }
+}
---
+++ Sorteio/Program.cs
@@ -0,0 +1,32 @@
+﻿using Sorteio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sorteio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Participante> list = new List<Participante>();
+
+            list.Add(new Participante(100, "Freedy Carvalho"));
+            list.Add(new Participante(200, "Raphael Carvalho"));
+            list.Add(new Participante(300, "Bruno Carvalho"));
+            list.Add(new Participante(400, "Francis Carvalho"));
+            list.Add(new Participante(500, "Vanessa Moro"));
+            list.Add(new Participante(600, "Eliane Martins"));
+
+
+            //var sorteado = list.OrderBy(x=> x.Nome).Select(x => x.Nome).Take(1);
+            var sorteado = list.OrderBy(n => Guid.NewGuid()).Take(2).Select(n => n.Nome);
+
+            foreach (string item in sorteado)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
